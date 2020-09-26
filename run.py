f50e0dca33ce71a2895f68c851636ab46e2851c6
Jhern603 <Jhern603@fiu.edu>
2020-09-26 16:00:20 -0400

    First Attempt at dialogFlow Integration

--- run.py
+++ run.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template
+#If you see this, then I did something right.
+from flask import Flask, request, jsonify, render_template
+from google.cloud import bigquery
+import os
+import dialogflow
+import requests
+import pusher
+import datetime
+import json
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route('/bq', methods=['GET'])
+def send():
+    destination = 'New York'
+    date = datetime.date(2020, 9, 23)
+    
+    #Perform a query.
+    QUERY = (
+        'SELECT * FROM `bigquery-public-data.covid19_public_forecasts.county_14d` '
+        f'WHERE state_name = "{destination}" '
+        'AND prediction_date > forecast_date '
+        'ORDER BY prediction_date '
+        'LIMIT 100')
 
+    result = query(QUERY)
+    return jsonify(result)
+    
+# POST example template
+# @app.route('/', methods=['POST'])
+# def update_record():
+#     record = json.loads(request.data)
+#     new_records = []
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#         records = json.loads(data)
+#     for r in records:
+#         if r['name'] == record['name']:
+#             r['email'] = record['email']
+#         new_records.append(r)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(new_records, indent=2))
+#     return jsonify(record)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=80)
