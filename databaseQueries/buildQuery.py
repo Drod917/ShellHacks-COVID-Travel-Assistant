990772d870af0160283af1d6cab483a9ca69804d
Jose Luis Zuniga <zunigajl@stthom.edu>
2020-09-26 10:53:11 -0500

    playing around with the query. BigQuery is being dumb. :/ But the request is going through fine with the API and key

--- databaseQueries/buildQuery.py
+++ databaseQueries/buildQuery.py
@@ -3,26 +3,34 @@
 # - request from bigQuery database
 # - return the result to be handled by interpretQUery
 
-from google.cloud import bigquery as bq
+from google.cloud import bigquery
 
 def stateQuery(state) :
 
-    client = bigquery.client
+    client = bigquery.Client.from_service_account_json("ShellHacks2020-487e58d8d077.json")
     query_job = client.query(
         """
         SELECT *
         FROM `bigquery-public-data.covid19_public_forecasts.state_14d`
-        WHERE state_name = @state
-        """
-     return(query_job)
+        WHERE state_name = """ + state
+
+    )
+    print("The query data:")
+    for row in query_job:
+        # Row values can be accessed by field name or index.
+        print("name={}, count={}".format(row[0], row["total_people"]))
+    return()
 
 
-def countyQuery(county, state) :
-    client = bigquery.client
-    query_job = client.query(
-        """
-        SELECT *
-        FROM `bigquery-public-data.covid19_public_forecasts.state_14d`
-        WHERE state_name = @state 
-        """
-    return(query_job))
+# def countyQuery(county, state) :
+#     client = bigquery.Client.from_service_account_json(ShellHacks2020-487e58d8d077.json)
+#     query_job = client.query(
+#         """
+#         SELECT *
+#         FROM `bigquery-public-data.covid19_public_forecasts.state_14d`
+#         WHERE state_name = @state
+#         """
+#     )
+#     return(query_job)
+
+stateQuery("Texas")
